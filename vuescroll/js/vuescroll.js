552d2d952080eefaca9fd5241b55ec1b64e30ed0
wangyi7099 <wy564654798@126.com>
2017-12-27 08:50:23 +0800

    fix:when  two scrollbars are folded together
    When tow scrollbars are folded together, the one would cover another one  that the 'showBar' method couldn't be trigger.

--- vuescroll/js/vuescroll.js
+++ vuescroll/js/vuescroll.js
@@ -1,5 +1,5 @@
 /*
- * vuescroll 1.0 
+ * vuescroll 1.4.5 
  * @author:wangyi qq:724003548
  * @date 2017年7月19日12:16:41
  * 参照着基于jQuery的simscroll所做的基于vue的滚动条插件
@@ -28,38 +28,8 @@
             Vue.component(vueScroll.name, vueScroll);            
         }
     };
-
-    //用来计算内容高度的wrap
-    var vueScrollCon = {
-        name: 'vueScrollCon',
-        render: function(createElement) {
-            var self = this;
-
-            bus.id1 = self.id1;
-          //  console.log(this)
-            return createElement('div', {
-                style: self.scrollPanelHeight
-                ,
-                attrs: {
-                    id: this.id1
-                },
-                on: {
-                    mouseenter: function() {
-
-                        bus.$emit('getbarHeight' + self.id1);
-                    }
-                }
-            }, this.$slots.default);
-        },
-        props:['scrollPanelHeight'],
-        data: function() {
-            return {
-                id1: "_ScrollCon" + new Date().valueOf()
-            };
-        }
-    }
-    //所滚动的div
-    var vuePanel = {
+     //scrollpanne
+     var vuePanel = {
         name: 'vueScrollpanel',
         render: function(createElement) {
             var self = this;
@@ -73,6 +43,11 @@
                 },
                 attrs: {
                     id: self.id
+                },
+                on: {
+                    mouseenter: function() {
+                        bus.$emit('getbarHeight' + self.id);
+                    }
                 }
             }, this.$slots.default);
         },
@@ -82,7 +57,31 @@
             };
         }
     }
-    //滚动条的样式。 可自行配置。
+    //scrollCon 高度和内容高度保持一致
+    var vueScrollCon = {
+        name: 'vueScrollCon',
+        render: function(createElement) {
+            var self = this;
+
+            bus.id1 = self.id1;
+          //  console.log(this)
+            return createElement('div', {
+                style: self.scrollContentStyle
+                ,
+                attrs: {
+                    id: this.id1
+                }
+            }, this.$slots.default);
+        },
+        props:['scrollContentStyle'],
+        data: function() {
+            return {
+                id1: "_ScrollCon" + new Date().valueOf()
+            };
+        }
+    }
+   
+    //滚动条 样式等参数可自行配置。
     var scrollBar = {
         name: 'scrollBar',
         render: function(createElement) {
@@ -131,64 +130,80 @@
                     id2:"_ScrollBar" + new Date().valueOf()
                 },
                 innerdeltaY: 0,
-                scrollElement: "",
-                scrollInner: "",
-                scrollSelf:"",
-                scrollHeight: "",
-                scrollInnerHeight: "",
+                scrollPanel: "",
+                scrollContent: "",
+                scrollBar:"",
+                scrollPanelHeight: "",
+                scrollPanelScrollHeight: "",
                 minBarHeight: 35,
-                mousedown: false
+                mousedown: false,
+                listeners:[]
             }
             //deltal 每次滑动的距离
         },
         props: ['ops'],
         methods: {
             getBarHeight: function() {
-                this.scrollHeight = window.getComputedStyle(this.scrollElement).getPropertyValue("height").replace('px', "");
-                this.scrollInnerHeight = this.scrollElement.scrollHeight;
+                this.scrollPanelHeight = window.getComputedStyle(this.scrollPanel).getPropertyValue("height").replace('px', "");
+                this.scrollPanelScrollHeight = this.scrollPanel.scrollHeight;
                 //在每次滚动this.deltaY的情况下滚动完剩余部分所需要的次数
-                var scrollTime = Math.ceil((this.scrollInnerHeight - this.scrollHeight) / Math.abs(this.options.deltaY));
+                var scrollTime = Math.ceil((this.scrollPanelScrollHeight - this.scrollPanelHeight) / Math.abs(this.options.deltaY));
                 //选择合适的滚动条大小
-                this.height = Math.max(this.scrollHeight / (this.scrollInnerHeight / this.scrollHeight), this.minBarHeight);
+                this.height = Math.max(this.scrollPanelHeight / (this.scrollPanelScrollHeight / this.scrollPanelHeight), this.minBarHeight);
+                if(this.scrollPanelScrollHeight <= this.scrollPanelHeight){
+                    this.height = 0;
+                }
                 //计算滚动条每次滚动的距离innerdeltaY
-                this.innerdeltaY = (this.scrollHeight - this.height) / scrollTime;
+                this.innerdeltaY = (this.scrollPanelHeight - this.height) / scrollTime;
                 //调整top的值
                 this.resizeTop();
                 this.showBar();
             },
             resizeTop: function() {
                 //先求出con剩余的值
-                var lastHeight = this.scrollInnerHeight - this.scrollHeight - this.scrollElement.scrollTop;
+                var lastHeight = this.scrollPanelScrollHeight - this.scrollPanelHeight - this.scrollPanel.scrollTop;
                 var time = Math.abs(Math.ceil(lastHeight / this.options.deltaY));
-                this.top = this.scrollHeight - (this.height + (time * this.innerdeltaY));
+                this.top = this.scrollPanelHeight - (this.height + (time * this.innerdeltaY));
             },
             showBar: function() {
-                if(this.scrollHeight < this.scrollInnerHeight){
-                    var bar = this.scrollSelf;
+                if(this.scrollPanelHeight < this.scrollPanelScrollHeight){
+                    var bar = this.scrollBar;
                     bar.style.opacity = 1;
                 }
             },
             hideBar: function() {
                 if (!this.mousedown) {
-                    var bar = this.scrollSelf
+                    var bar = this.scrollBar
                     bar.style.opacity = 0;
                 }
             },
             listenmouseout: function() {
                 var self = this;
-                self.$el.parentNode.addEventListener('mouseleave', function(e) {
+                function t() {
                     bus.$emit('hidebar');
+                }
+                this.listeners.push({
+                    dom:self.$el.parentNode,
+                    event:t,
+                    type:"mouseleave"
                 });
+                self.$el.parentNode.addEventListener('mouseleave', t);
             },
             //监听滚轮事件
             listenwheel: function() {
                 var self = this;
-                self.$el.parentNode.addEventListener('wheel', function(e) {
+                function t(e) {
                     //console.log(e.deltaY);
                     self.getBarHeight();
                     //
                     self.scrollCon(e.deltaY > 0 ? 1 : -1, 1);
+                }
+                this.listeners.push({
+                    dom:self.$el.parentNode,
+                    event:t,
+                    type:"wheel"
                 });
+                self.$el.parentNode.addEventListener('wheel',t);
             },
             //监听拖拽滚动条的事件
             listenDrag: function() {
@@ -203,7 +218,7 @@
                     y = _y;
 
                 }
-                self.$el.addEventListener('mousedown', function(e) {
+                function t(e) {
                     //console.log(e);
                     self.mousedown = true;
                     y = e.pageY;
@@ -215,28 +230,34 @@
                         document.removeEventListener('mousemove', move);
 
                     });
+                }
+                this.listeners.push({
+                    dom:self.$el,
+                    event:t,
+                    type:"mousedown"
                 });
+                self.$el.addEventListener('mousedown',t);
             },
             scrollCon: function(pos, time) {
                 //pos：方向   1：向下滚动  0：向上滚动
 
-                if (!((pos < 0 && this.top <= 0) || (this.scrollHeight <= this.top + this.height && pos > 0))) {
+                if (!((pos < 0 && this.top <= 0) || (this.scrollPanelHeight <= this.top + this.height && pos > 0))) {
                     var Top = this.top + pos * this.innerdeltaY * time;
-                    var ScrollTop = this.scrollElement.scrollTop + pos * this.options.deltaY * time;
+                    var ScrollTop = this.scrollPanel.scrollTop + pos * this.options.deltaY * time;
                     if (pos < 0) {
                         //向上滚的
                         this.top = Math.max(0, Top);
-                        this.scrollElement.scrollTop = Math.max(0, ScrollTop);
+                        this.scrollPanel.scrollTop = Math.max(0, ScrollTop);
                     } else if (pos > 0) {
                         //向下滚得
-                        this.top = Math.min(this.scrollHeight - this.height, Top);
-                        this.scrollElement.scrollTop = Math.min(this.scrollInnerHeight - this.scrollHeight, ScrollTop);
+                        this.top = Math.min(this.scrollPanelHeight - this.height, Top);
+                        this.scrollPanel.scrollTop = Math.min(this.scrollPanelScrollHeight - this.scrollPanelHeight, ScrollTop);
                     }
                 }
                 var content = {};
                 var bar = {};
-                content.lastScrolled = (this.scrollElement.scrollHeight - this.scrollElement.scrollTop) + 'px';
-                content.hasScrolled = this.scrollElement.scrollTop + 'px';
+                content.lastScrolled = (this.scrollPanel.scrollHeight - this.scrollPanel.scrollTop) + 'px';
+                content.hasScrolled = this.scrollPanel.scrollTop + 'px';
                 bar.hasScrolled = this.sTop;
                 bar.height = this.sHeight;
                 bar.lastScrolled = this.barlastScrolled;
@@ -261,23 +282,32 @@
                 return this.height + 'px';
             },
             barlastScrolled: function() {
-                return (this.scrollHeight - this.top - this.height) + 'px';
+                return (this.scrollPanelHeight - this.top - this.height) + 'px';
             }
 
         },
-
+        beforeCreate(){
+            var self = this;
+              
+        },
         mounted: function() {
             var self = this;
-            self.scrollElement = document.getElementById(self.ids.id);
-            self.scrollInner = document.getElementById(self.ids.id1);
-            self.scrollSelf = document.getElementById(self.ids.id2);
-            bus.$on('getbarHeight' + self.ids.id1, self.getBarHeight);
+            self.scrollPanel = document.getElementById(self.ids.id);
+            self.scrollContent = document.getElementById(self.ids.id1);
+            self.scrollBar = document.getElementById(self.ids.id2);
+            bus.$on('getbarHeight' + self.ids.id, self.getBarHeight);
             bus.$on('hidebar', self.hideBar);
             self.merge(self.options, self.ops);
-            self.getBarHeight();
             self.listenwheel();
             self.listenDrag();
             self.listenmouseout();
+            self.getBarHeight();
+        },
+        beforeDestroy(){
+            //
+            this.listeners.map((element) => {
+                element.dom.removeEventListener(element.type,element.event);
+            })
         }
     }
     var vueScroll = {
@@ -296,7 +326,7 @@
 
                 },[createElement('vueScrollCon',{
                     props:{
-                        scrollPanelHeight:self.scrollPanelHeight
+                        scrollContentStyle:self.scrollContentStyle
                     }
                 },self.$slots.default)]),  
                 createElement('scrollBar',{
@@ -316,7 +346,7 @@
             scroll:{
                 require:false
             } ,
-            scrollPanelHeight:{
+            scrollContentStyle:{
                 require:false
             }  
         }  
