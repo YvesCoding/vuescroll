53e6eb314ebba1d696544a245f24120d397f3a24
wangyi7099 <wy564654798@126.com>
2018-05-14 00:03:15 +0800

    build: release 4.5.16 chore: add unit test fix: window onresize may throw an error

--- dist/vuescroll.js
+++ dist/vuescroll.js
@@ -1,5 +1,5 @@
 /*
-    * @name: vuescroll 4.5.15
+    * @name: vuescroll 4.5.16
     * @author: (c) 2018-2018 wangyi7099
     * @description: A reactive virtual scrollbar based on vue.js 2.X
     * @license: MIT
@@ -3479,7 +3479,7 @@ var scroll = {
 
     Vue$$1.prototype.$vuescrollConfig = deepMerge(GCF, {});
     scroll.isInstalled = true;
-    scroll.version = '4.5.15';
+    scroll.version = '4.5.16';
   }
 };
 /* istanbul ignore if */
