5776d2432d175d25fc1e6791d07e1022c796dcd3
wangyi7099 <wy564654798@126.com>
2018-05-23 01:53:33 +0800

    build: 4.5.19

--- dist/vuescroll.esm.js
+++ dist/vuescroll.esm.js
@@ -1,5 +1,5 @@
 /*
-    * @name: vuescroll 4.5.18
+    * @name: vuescroll 4.5.19
     * @author: (c) 2018-2018 wangyi7099
     * @description: A reactive virtual scrollbar based on vue.js 2.X
     * @license: MIT
@@ -3583,7 +3583,7 @@ var Vuescroll = {
     Vue$$1.prototype.$vuescrollConfig = deepMerge(GCF, {});
   },
 
-  version: '4.5.18'
+  version: '4.5.19'
 };
 
 /* istanbul ignore if */
