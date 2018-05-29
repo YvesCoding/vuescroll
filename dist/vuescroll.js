9eadd214f2a8777b9f90220675691686731fe6e6
wangyi7099 <wy564654798@126.com>
2018-05-29 19:09:18 +0800

    chore: changelog

--- dist/vuescroll.js
+++ dist/vuescroll.js
@@ -190,8 +190,12 @@ function listenResize(element, callback) {
 }
 
 function injectObject(element, callback) {
+  if (element.hasResized) {
+    return;
+  }
   var OBJECT_STYLE = 'display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none; padding: 0; margin: 0; opacity: 0; z-index: -1000; pointer-events: none;';
   var object = document.createElement('object');
+  element.hasResized = true;
   object.style.cssText = OBJECT_STYLE;
   object.tabIndex = -1;
   object.type = 'text/html';
@@ -206,6 +210,7 @@ function injectObject(element, callback) {
       eventCenter(object.contentDocument.defaultView, 'resize', callback, 'off');
     }
     element.removeChild(object);
+    element.hasResized = false;
   };
 }
 
--- src/third-party/resize-detector/index.js
+++ src/third-party/resize-detector/index.js
@@ -5,9 +5,13 @@ export function listenResize(element, callback) {
 }
 
 function injectObject(element, callback) {
+  if(element.hasResized) {
+    return;
+  }
   var OBJECT_STYLE =
     'display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none; padding: 0; margin: 0; opacity: 0; z-index: -1000; pointer-events: none;';
   var object = document.createElement('object');
+  element.hasResized = true;
   object.style.cssText = OBJECT_STYLE;
   object.tabIndex = -1;
   object.type = 'text/html';
@@ -27,5 +31,6 @@ function injectObject(element, callback) {
       );
     }
     element.removeChild(object);
+    element.hasResized = false;
   };
 }
