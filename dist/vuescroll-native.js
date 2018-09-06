0da452ccb71eed5f22534bc96239bb0fef98c4fb
wangyi7099 <724003548@qq.com>
2018-09-06 20:32:23 +0800

    build: release 4.8.4

--- dist/vuescroll-native.js
+++ dist/vuescroll-native.js
@@ -1,5 +1,5 @@
 /*
-    * Vuescroll v4.8.2
+    * Vuescroll v4.8.4
     * (c) 2018-2018 Yi(Yves) Wang
     * Released under the MIT License
     * Github: https://github.com/YvesCoding/vuescroll
@@ -1990,7 +1990,7 @@ function install(Vue$$1) {
 
 var Vuescroll = {
   install: install,
-  version: '4.8.2',
+  version: '4.8.4',
   refreshAll: refreshAll
 };
 
