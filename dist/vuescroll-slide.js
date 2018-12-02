0695b3214cc4f5f002f1c58645bae08735367ed4
wangyi7099 <724003548@qq.com>
2018-12-02 12:31:31 +0800

    build: release 4.9.0-beta.17

--- dist/vuescroll-slide.js
+++ dist/vuescroll-slide.js
@@ -1,5 +1,5 @@
 /*
-    * Vuescroll v4.9.0-beta.16
+    * Vuescroll v4.9.0-beta.17
     * (c) 2018-2018 Yi(Yves) Wang
     * Released under the MIT License
     * Github: https://github.com/YvesCoding/vuescroll
@@ -4401,7 +4401,7 @@ function install(Vue$$1) {
 
 var Vuescroll = {
   install: install,
-  version: '4.9.0-beta.16',
+  version: '4.9.0-beta.17',
   refreshAll: refreshAll,
   scrollTo: scrollTo
 };
