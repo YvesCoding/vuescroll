8353852ec9e65c2b0e02aa4f78133831ff91f5de
wangyi <724003548@qq.com>
2019-02-01 15:57:23 +0800

    fix: Remove undefined value in entry point of vuescroll-slide

--- src/mode/slide/index.js
+++ src/mode/slide/index.js
@@ -4,13 +4,7 @@ import { config, configValidator } from './config';
 
 import { _install } from 'mode/shared/util';
 
-const component = _install(
-  [scrollPanel, bar],
-  core,
-  render,
-  config,
-  configValidator
-);
+const component = _install(core, render, config, configValidator);
 
 export default function install(Vue, opts = {}) {
   Vue.component(opts.name || component.name, component);
