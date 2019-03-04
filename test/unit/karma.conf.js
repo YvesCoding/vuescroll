f04623361a21ea5f9a876af091cde8ae0b5c6f82
wangyi <724003548@qq.com>
2019-03-04 11:39:16 +0800

    chore: remove useless dependencies.

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -22,14 +22,6 @@ var webpack = {
             ]
           ]
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader' // compiles Sass to CSS
-        ]
       }
     ]
   },
