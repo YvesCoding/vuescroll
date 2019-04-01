bb0d84294444972e7bf6bfb4876576e030a42283
wangyi7099 <724003548@qq.com>
2019-04-01 20:57:32 +0800

    fix: make linter happay.

--- src/core/third-party/resize-detector/index.js
+++ src/core/third-party/resize-detector/index.js
@@ -1,5 +1,5 @@
 // detect content size change
-import { eventCenter, isIE, isIos } from 'shared/util';
+import { eventCenter, isIE } from 'shared/util';
 export function installResizeDetection(element, callback) {
   return injectObject(element, callback);
 }
