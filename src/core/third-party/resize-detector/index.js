bf2a01b9ad335e7a6605324371c21c9a8fd9c465
wangyi7099 <724003548@qq.com>
2019-04-01 20:38:38 +0800

    fix(ios): Mounted twice in iphone.(close #116)

--- src/core/third-party/resize-detector/index.js
+++ src/core/third-party/resize-detector/index.js
@@ -18,15 +18,12 @@ function injectObject(element, callback) {
   object.style.cssText = OBJECT_STYLE;
   object.type = 'text/html';
   object.tabIndex = -1;
-  // Set # to make it work on safari mobile
-  if (isIos()) {
-    object.data = '#';
-  }
+
   object.onload = () => {
     eventCenter(object.contentDocument.defaultView, 'resize', callback);
   };
   // https://github.com/wnr/element-resize-detector/blob/aafe9f7ea11d1eebdab722c7c5b86634e734b9b8/src/detection-strategy/object.js#L159
-  if (!isIE() && !isIos()) {
+  if (!isIE()) {
     object.data = 'about:blank';
   }
   objWrap.isResizeElm = true;
