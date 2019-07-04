e7d2fc1998f70b3729fdadad1300b491a44b27de
wangyi <724003548@qq.com>
2019-07-04 10:35:59 +0800

    build: release 4.13.1

--- dist/vuescroll.js
+++ dist/vuescroll.js
@@ -1,5 +1,5 @@
 /*
-    * Vuescroll v4.13.0
+    * Vuescroll v4.13.1
     * (c) 2018-2019 Yi(Yves) Wang
     * Released under the MIT License
     * Github: https://github.com/YvesCoding/vuescroll
@@ -4938,7 +4938,7 @@ function install(Vue$$1) {
 
 var Vuescroll = _extends({
   install: install,
-  version: '4.13.0',
+  version: '4.13.1',
   refreshAll: refreshAll,
   scrollTo: scrollTo
 }, component);
